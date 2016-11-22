ed5124305750eedd1f18290d4d58755e00355730
SurajAnthwal <suraj@zemosolabs.com>
2016-11-23 00:59:54 +0530

    Callbacks and task till 6pts completed

--- Request.js
+++ Request.js
@@ -1,19 +1,23 @@
 var request = require('superagent');
 
 module.exports = {
-    getDefinition: function (word) {
+    getDefinition: function (word, callback) {
         request
             .get("http://api.wordnik.com:80/v4/word.json/" + word + "/definitions")
             .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
             .set('Accept', 'application/json')
             .end(function (err, res) {
+                console.log("DEFINITIONS: ")
                 for (var i = 0; i < res.body.length; i++) {
                     console.log(res.body[i].text);
-                    console.log("\t" + res.body[i].attributionText + "\n");
+                    console.log("\t" + res.body[i].attributionText);
                 }
+                console.log("\n");
+                if (typeof callback == "function")
+                    callback();
             });
     },
-    getSynonym: function (word) {
+    getSynonym: function (word, callback) {
         var synonyms = [];
         request
             .get("http://api.wordnik.com:80/v4/word.json/" + word + "/relatedWords")
@@ -21,9 +25,11 @@ module.exports = {
             .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
             .set('Accept', 'application/json')
             .end(function (err, res) {
+
                 if (res.body.length == 0)
-                    console.log("No synonym");
+                    console.log("NO Synonym" + "\n");
                 else {
+                    console.log("SYNONYMS: ")
                     synonyms = res.body[0].words;
                     for (i = 0; i < synonyms.length; i++) {
                         if (synonyms.length > 1)
@@ -31,10 +37,13 @@ module.exports = {
                         else
                             console.log(synonyms[i]);
                     }
+                    console.log("\n");
                 }
+                if (typeof callback == "function")
+                    callback();
             });
     },
-    getAntonym: function (word) {
+    getAntonym: function (word, callback) {
         var antonyms = [];
         request
             .get("http://api.wordnik.com:80/v4/word.json/" + word + "/relatedWords")
@@ -43,7 +52,7 @@ module.exports = {
             .set('Accept', 'application/json')
             .end(function (err, res) {
                 if (res.body.length == 0)
-                    console.log("No antonym");
+                    console.log("NO antonym" + "\n");
                 else {
                     antonyms = res.body[0].words;
                     for (i = 0; i < antonyms.length; i++) {
@@ -52,7 +61,10 @@ module.exports = {
                         else
                             console.log(antonyms[i]);
                     }
+                    console.log("\n");
                 }
+                if (typeof callback == "function")
+                    callback();
             });
     },
     getExample: function (word) {
@@ -63,15 +75,18 @@ module.exports = {
             .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
             .set('Accept', 'application/json')
             .end(function (err, res) {
+                console.log("EXAMPLES: ");
                 if (res.body.examples) {
                     examples = res.body.examples;
                     for (i = 0; i < examples.length; i++) {
                         console.log(examples[i].text);
                     }
+                    console.log("\n");
                 }
+                return 1;
             });
     },
-    getWordOfDay: function () {
+    getWordOfDay: function (callback) {
         var d = new Date();
         var year = d.getFullYear();
         var month = d.getMonth() + 1;
@@ -84,8 +99,12 @@ module.exports = {
             .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
             .set('Accept', 'application/json')
             .end(function (err, res) {
+                console.log("WORD OF THE DAY: ");
                 if (res.body.word)
-                    return res.body.word;
+                    console.log(res.body.word);
+                // console.log("\n");
+                if (typeof callback == "function")
+                    callback(res.body.word);
             });
     }
 
--- index.js
+++ index.js
@@ -10,15 +10,23 @@ var program = require('commander');
 var co = require('co');
 var prompt = require('co-prompt');
 var Constants = require('./Constants');
-var Request = require('./Request')
+var Request = require('./Request');
 // console.log(" constant is " + Constants.DEFINITION);
 
 var getAllWordDetails = function (word) {
 
-    Request.getDefinition(word);
-    Request.getSynonym(word);
-    Request.getAntonym(word);
-    Request.getExample(word);
+    Request.getDefinition(word,getSynonym);
+
+    function getSynonym() {
+        Request.getSynonym(word,getAntonym);
+    }
+    function getAntonym() {
+        Request.getAntonym(word,getExample);
+    }
+
+    function getExample() {
+        Request.getExample(word);
+    }
 
 };
 
@@ -26,18 +34,18 @@ if (userArgs[1])
     userArgs[1] = userArgs[1].trim();
 
 if (userArgs[0] == Constants.DEFINITION) {
-    Request.getDefinition(userArgs[1]);
+    Request.getDefinition(userArgs[1],null);
 } else if (userArgs[0] == Constants.SYNONYM) {
-    Request.getSynonym(userArgs[1]);
+    Request.getSynonym(userArgs[1],null);
 
 } else if (userArgs[0] == Constants.ANTONYM) {
-    Request.getAntonym(userArgs[1]);
+    Request.getAntonym(userArgs[1],null);
 
 } else if (userArgs[0] == Constants.EXAMPLE) {
-    Request.getExample(userArgs[1]);
+    Request.getExample(userArgs[1],null);
 } else if (userArgs.length == 0) {
-    getAllWordDetails(Request.getWordOfDay());
-    
+    Request.getWordOfDay(getAllWordDetails);
+
 
 } else if (userArgs.length != 0) {
     getAllWordDetails(userArgs[0]);
