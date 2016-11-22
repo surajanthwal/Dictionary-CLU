915854b47d792ca120c745960b16f1795d7d823d
SurajAnthwal <suraj@zemosolabs.com>
2016-11-22 23:26:56 +0530

    First Phase of Application completed nearly

--- index.js
+++ index.js
@@ -41,13 +41,62 @@ if (userArgs[0] == Constants.DEFINITION) {
                 }
             }
             for (i = 0; i < synonyms.length; i++) {
-                if(synonyms.length>1)
-                console.log(synonyms[i]+",");
-            else 
+                if (synonyms.length > 1)
+                    console.log(synonyms[i] + ",");
+                else
                     console.log(synonyms[i]);
             }
         });
 
+} else if (userArgs[0] == Constants.ANTONYM) {
+    var antonyms = [];
+    request
+        .get("http://api.wordnik.com:80/v4/word.json/" + userArgs[1] + "/relatedWords")
+        .query({relationshipTypes: 'antonym'})
+        .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+            if (res.body[0].words) {
+                antonyms = res.body[0].words;
+                for (i = 0; i < antonyms.length; i++) {
+                    if (antonyms.length > 1)
+                        console.log(antonyms[i] + ",");
+                    else
+                        console.log(antonyms[i]);
+                }
+            }
+        });
+} else if (userArgs[0] == Constants.EXAMPLE) {
+    var examples = [];
+    request
+        .get("http://api.wordnik.com:80/v4/word.json/" + userArgs[1] + "/examples")
+        // .query({relationshipTypes: 'antonym'})
+        .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+            if (res.body.examples) {
+                examples = res.body.examples;
+                for (i = 0; i < examples.length; i++) {
+                    console.log(examples[i].text);
+                }
+            }
+        });
+} else {
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var date = year + "-" + month + "-" + day;
+
+    request
+        .get("http://api.wordnik.com:80/v4/words.json/wordOfTheDay")
+        .query({date: date})
+        .set("api_key", "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+            if (res.body.word)
+                console.log(res.body.word);
+        });
 }
 
 
